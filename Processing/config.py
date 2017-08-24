c112bf3cf4a9d258fdee21687a496084bb66643f
Mengyuan Liu <liumyzoe@uw.edu>
2017-08-24 13:26:01 -0700

    add processing folder with code for processing

---
+++ Processing/config.py
@@ -0,0 +1,12 @@
+# Creditials Contants
+
+# INSTRUCTIONS
+# 1. Replace all variables with appropriate the appropriate values.
+# 2. Rename file from config_example.py to config.py
+
+## RDS on AWS
+DATABASE_NAME = "cherrypick_api_db"
+DATABASE_USERNAME = "cherrypick"
+DATABASE_PASSWORD = "kQ~y94h]={(hSJ*~"
+DATABASE_HOST = "insightcherrypick.cm5eybz7wx3n.us-east-1.rds.amazonaws.com"
+DATABASE_PORT = "5432"
